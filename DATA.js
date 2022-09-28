9af82e960f9d5041f826f39a3211ab82ec5b5c37
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:17:46 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "48569d60-5f6e-4039-ba09-58e026d960ec",
+    "home_feed": [
+        {
+            "id": "75828259-dd9f-4aae-9d81-2976b08bbb4f",
+            "artist_name": "listen",
+            "home_carousel": [
+                {
+                    "id": "824d70fd-fec2-4792-8db1-629207134913",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "f841b831-0354-4f4e-971c-a95c447f0702",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/152.png"
+                },
+                {
+                    "id": "f9d730ef-69c1-4353-8845-a64e771d568f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "75c8a3d5-d1c9-4ee1-9538-d04e243f46bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "30c4ba40-886a-4ce7-8590-7cb6f8757d32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png"
+                }
+            ],
+            "art_caption": "dog",
+            "art_description": "Number free toward. Law animal than forget writer. Girl wall environment why."
+        },
+        {
+            "id": "d532fb9a-2805-42c0-af93-8ea0f31606bb",
+            "artist_name": "scientist",
+            "home_carousel": [
+                {
+                    "id": "441bf506-3962-4d6b-8906-7de5b663b7d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/224.png"
+                },
+                {
+                    "id": "5bdd9ad0-518d-4c37-909e-3bda9ce1c5a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "7b79848a-511e-4dc2-a7f3-ac13f587291b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                },
+                {
+                    "id": "7946297c-3b3b-4b93-865d-1286a673c8b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "85b6c77b-ae92-4d43-ae54-9008bba928ab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                }
+            ],
+            "art_caption": "along",
+            "art_description": "That expect television present weight. Rock good nearly worker feel just meet boy. Marriage race measure which."
+        },
+        {
+            "id": "3d980af7-03ca-410a-92a4-c218b21fd7fe",
+            "artist_name": "thank",
+            "home_carousel": [
+                {
+                    "id": "db4aae1e-a9a2-4d24-a1be-9c585d0d5ea6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "c8a750b3-13f0-4328-ada0-f1cfe2e92d61",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "11378473-bf3b-4215-a2a8-70144d789611",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                },
+                {
+                    "id": "d7ee3ca0-4ba3-4e3d-b539-e5b396ee2e7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "897e12c4-1a74-4d42-a222-06cd7bf89e00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                }
+            ],
+            "art_caption": "mean",
+            "art_description": "Drug probably see event me important hand."
+        },
+        {
+            "id": "c7bebd44-4ace-498d-93c6-89fe3a0a3de7",
+            "artist_name": "same",
+            "home_carousel": [
+                {
+                    "id": "b35f36c9-4c5c-45ea-9787-1b0a07db951f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "560a0943-9d1b-4663-8300-08bad7cd7a7d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "b6051e49-400a-494b-a7f8-f9ec309cfe2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "4ecdbde2-a4a2-47fe-85d0-b3bb6d696f68",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "79d688d5-af5b-4331-b9e9-9de48f1cdc0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                }
+            ],
+            "art_caption": "production",
+            "art_description": "Tonight class area pretty mention close speak. Sound receive process size he investment under."
+        },
+        {
+            "id": "3a7e1871-dcda-4c66-8db5-49bb75ab7ad1",
+            "artist_name": "a",
+            "home_carousel": [
+                {
+                    "id": "2c2444ad-8849-4cb8-91a7-0f35072d700f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                },
+                {
+                    "id": "de6bb5ad-1eeb-46e9-b903-6f3e2e538333",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "e7d6ac9e-aba4-46d7-9384-1a29c5017e79",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/302.png"
+                },
+                {
+                    "id": "ec83fd87-32c0-49bc-8a4c-94f3a124b032",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "3f56b23b-56bb-43d8-baa9-0d33cfbe9d07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                }
+            ],
+            "art_caption": "sure",
+            "art_description": "Hot minute media industry. Thing exist arrive guy worry American market. New year create major feel attack beyond. Expect plan open behavior politics."
+        }
+    ]
+}
